e6980eb710c08df95ce8f7dc13598946870f1f92
Patrick Monteiro <eng.patrickmonteiro@gmail.com>
2016-10-06 12:35:48 -0300

    Blog parte 1

---
+++ application/views/artigos.php
@@ -0,0 +1,22 @@
+<html>
+	<body>
+		<h2>Artigos do nosso blog</h2>
+
+		<table>
+			<tr>
+				<th>Titulo</th>
+				<th>Conteúdo</th>
+			</td>
+
+			<tr>
+				<td>Artigo PHP</td>
+				<td>Nosso primeiro artigo php</td>
+			</tr>
+
+			<tr>
+				<td>Artigo Codeigniter</td>
+				<td>Nosso poderoso Framework WEB</td>
+			</tr>
+		</table>
+	</body>
+</html>
